4a0b947465f903abca457f6e5a93a7429184d2d8
vivek <chimnanivivek14@gmail.com>
2024-02-11 16:40:01 +0530

    Meals Page

---
+++ components/meals/meal-item.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Image from "next/image";
+import classes from "./meal-item.module.css";
+
+export default function MealItem({ title, mealId, image, summary, creator }) {
+  return (
+    <article className={classes.meal}>
+      <header>
+        <div className={classes.image}>
+          <Image src={image} alt={title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h2>{title}</h2>
+          <p>by {creator}</p>
+        </div>
+      </header>
+      <div className={classes.content}>
+        <p className={classes.summary}>{summary}</p>
+        <div className={classes.actions}>
+          <Link href={`/meals/${mealId}`}>View Details</Link>
+        </div>
+      </div>
+    </article>
+  );
+}
