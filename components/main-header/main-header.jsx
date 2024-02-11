69946fde098fa9127b47142bd13f4f155c6f6694
vivek <chimnanivivek14@gmail.com>
2024-02-11 16:18:17 +0530

    using cilent components efficiently

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
   return (
@@ -17,10 +18,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
