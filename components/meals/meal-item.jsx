d466c50cd4ac297ffcfeeb139c156fc9d71a7fa2
vivek <chimnanivivek14@gmail.com>
2024-02-12 10:32:27 +0530

    added sqlite3 db

--- components/meals/meal-item.jsx
+++ components/meals/meal-item.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import classes from "./meal-item.module.css";
 
-export default function MealItem({ title, mealId, image, summary, creator }) {
+export default function MealItem({ title, slug, image, summary, creator }) {
   return (
     <article className={classes.meal}>
       <header>
@@ -17,7 +17,7 @@ export default function MealItem({ title, mealId, image, summary, creator }) {
       <div className={classes.content}>
         <p className={classes.summary}>{summary}</p>
         <div className={classes.actions}>
-          <Link href={`/meals/${mealId}`}>View Details</Link>
+          <Link href={`/meals/${slug}`}>View Details</Link>
         </div>
       </div>
     </article>
